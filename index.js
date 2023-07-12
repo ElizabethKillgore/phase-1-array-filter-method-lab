f3b2a301976b0eb5118786201d68c07343b84601
Elizabeth <ek@elizabethkillgore.com>
2023-07-12 17:41:20 -0500

    arrayfilter lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  const filteredArray = drivers.filter(function (driver) {
+        return driver.toLowerCase() === name.toLowerCase()
+})  
+
+return filteredArray
+
+}
+
+function fuzzyMatch(drivers, firstLetterName) {
+    
+    const filteredDrivers = drivers.filter(function (driver){
+        if (firstLetterName === driver.substring(0, 2)) {
+            return driver
+         } else {
+            return undefined
+            }
+        })
+  return filteredDrivers
+        
+}
+
+function matchName(drivers, name) {
+    const filterObjects = drivers.filter(function (object) {
+        console.log(object.name)
+        if (name === object.name) {
+            return object
+        } else {
+            return undefined
+        }
+        
+    })
+    return filterObjects
+}
